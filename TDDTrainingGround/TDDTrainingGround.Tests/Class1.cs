524d5f3b9141ac745655198cf96974dc2d33b631
Michał Chamów <michalchamow@gmail.com>
2015-10-25 19:25:22 +0100

    Schema

---
+++ TDDTrainingGround/TDDTrainingGround.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDTrainingGround.Tests
+{
+    public class Class1
+    {
+    }
+}
