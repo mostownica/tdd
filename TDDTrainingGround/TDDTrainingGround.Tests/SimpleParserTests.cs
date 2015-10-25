fb7b188409e7d5ec955ede7748794f46c594e6b6
Michał Chamów <michalchamow@gmail.com>
2015-10-25 19:30:13 +0100

    Simple Parser class

--- TDDTrainingGround/TDDTrainingGround.Tests/SimpleParserTests.cs
+++ TDDTrainingGround/TDDTrainingGround.Tests/SimpleParserTests.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TDDTrainingGround
+namespace TDDTrainingGround.Tests
 {
-    public class Class1
+    public class SimpleParserTests
     {
     }
 }
---
+++ TDDTrainingGround/TDDTrainingGround/SimpleParser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDTrainingGround
+{
+    public class SimpleParser
+    {
+        public int ParseAndSum(string numbers)
+        {
+            
+        }
+
+    }
+}
